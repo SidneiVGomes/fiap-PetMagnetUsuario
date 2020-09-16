0feaf838729485895bcb6efd1a414775a698125b
SidneiVGomes <sidneivgomes@gmail.com>
2020-09-16 02:18:17 -0300

    Implementando a tela de Publicações

--- views/Publicacoes.js
+++ views/Publicacoes.js
@@ -1,14 +1,49 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
-import {appStyles} from '../assets/css/AppStyle';
+import { Text, View, SafeAreaView, FlatList, StyleSheet, List, ListItem } from 'react-native';
+import { appStyles } from '../assets/css/AppStyle';
+import { ActivityIndicator } from 'react-native';
 
 export default function Publicacoes() {
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://petmagnet-api.herokuapp.com/API/publicacoes/proximas')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch((error) => setLoading(false));
+
+    console.log(data);
+    console.log(isLoading);
+    console.log('================================================================');
+  }, []);
+
+
   return (
     <View style={appStyles.container}>
       <Text>LISTA DE ANÚNCIOS PUBLICADOS</Text>
       <StatusBar style="auto" />
     </View>
+
+
+
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
